3114464884734a4e37ff38eeeaa08514d3ee7248
Kanha Goyal <goyal.abhyuday@gmail.com>
2021-03-05 10:53:18 +0000

    Player Movement done Making some jump

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+    public bool isFacingRight;
+
+    [Space]
+    public bool isGrounded;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+    public float checkRadius;
+
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>(); 
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb2d.AddForce(Vector2.up * jumpForce);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundLayer);
+
+
+        Move();
+    }
+
+    public void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+
+    public void Move()
+    {
+        moveInput = Input.GetAxis("Horizontal");
+
+        rb2d.velocity = new Vector2(moveInput * speed, rb2d.velocity.y);
+
+        if (isFacingRight && moveInput > 0)
+        {
+            Flip();
+        }
+        else if (!isFacingRight && moveInput < 0)
+        {
+            Flip();
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(groundCheck.position, checkRadius);
+    }
+
+}
