33fa6253b8fdb1c11580e0f20a4367ae015b9a3d
Kanha Goyal <goyal.abhyuday@gmail.com>
2021-03-05 12:56:57 +0000

    Made the Jump Functions

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -25,9 +25,10 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        //This line of code will control the jump power or DO the jump!!
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
         {
-            rb2d.AddForce(Vector2.up * jumpForce);
+            rb2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
     }
 
