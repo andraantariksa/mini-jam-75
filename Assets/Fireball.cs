6346b7d1bc71fb07968d046e791e15cd4643a8b5
Andra Antariksa <andra.antariksa@gmail.com>
2021-03-05 12:27:07 +0000

    Add sample fireball and stump

---
+++ Assets/Fireball.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    public float moveSpeed = 3f;
+    
+    void FixedUpdate()
+    {
+        transform.Translate(moveSpeed * Time.deltaTime, 0, 0);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Enemy
+    }
+}
