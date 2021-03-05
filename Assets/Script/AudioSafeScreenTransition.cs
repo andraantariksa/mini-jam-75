a041f3c8d344c1242cf9deea9549e860dfa0dd15
Andra Antariksa <andra.antariksa@gmail.com>
2021-03-05 10:35:43 +0000

    Add main menu scene

---
+++ Assets/Script/AudioSafeScreenTransition.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSafeScreenTransition : MonoBehaviour
+{
+    private static AudioClip audioClipPersistence;
+    private static AudioSafeScreenTransition instance = null;
+    public static AudioSafeScreenTransition Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+        if (instance != null && instance != this) {
+            Destroy(this.gameObject);
+            return;
+        } else {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void PlayOneShot(AudioClip audioClip)
+    {
+        audioClipPersistence = audioClip;
+        instance.GetComponent<AudioSource>().PlayOneShot(audioClipPersistence);
+    }
+}
---
+++ Assets/Script/AudioSourceMediator.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSourceMediator : MonoBehaviour
+{
+    public void PlayOneShot(AudioClip audioClip)
+    {
+        AudioSafeScreenTransition.Instance.PlayOneShot(audioClip);
+    }
+}
