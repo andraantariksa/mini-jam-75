cf5a0f7f633db88f0d74c7d30b04f509e5a20b5f
Andra Antariksa <andra.antariksa@gmail.com>
2021-03-08 00:08:03 +0000

    Main menu, enemy animation, and add some music

--- Assets/Script/MainMenuManager.cs
+++ Assets/Script/MainMenuManager.cs
@@ -5,8 +5,20 @@ using UnityEngine;
 
 public class MainMenuManager : MonoBehaviour
 {
+    public GameObject creditOverlay;
+
     public void PressPlay()
     {
         SceneManager.LoadScene("Scene/SampleScene", LoadSceneMode.Single);
+    }
+
+    public void OpenURL(string url)
+    {
+        Application.OpenURL(url);
+    }
+
+    public void ToggleCreditOverlay(bool show)
+    {
+        creditOverlay.SetActive(show);
     }
 }
